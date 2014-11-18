dea7cb4f86520c5115db3d4d9324cbf8be144680
Bryce Kerley <bkerley@brycekerley.net>
2014-11-17 19:48:52 -0500

    insult josef

--- 1-hello.rb
+++ 1-hello.rb
@@ -4,10 +4,25 @@ ADJECTIVES = %w{stinky smelly ugly alcoholic}
 NOUNS = %w{jerk doofus dingus developer}
 
 get '/' do
+  <<-EOF
+    <h1>Welcome to Miami Ruby Brigade</h1>
+    <p><a href="/insult">get insulted here, it's free!</a></p>
+    EOF
+end
+
+get '/insult' do
   insult = [ADJECTIVES.shuffle.first,
       NOUNS.shuffle.first].join ' '
 
   <<-EOF
   you're a #{insult}
+  EOF
+end
+
+get '/insult/:noun' do
+  insult = ADJECTIVES.shuffle.first
+
+  <<-EOF
+  you're #{insult}, #{params[:noun]}
   EOF
 end
