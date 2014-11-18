bb46cfbd7aeb345cc62db49d452c8e8693cd6783
Bryce Kerley <bkerley@brycekerley.net>
2014-11-17 20:00:05 -0500

    vladimir was hir

--- 1-hello.rb
+++ 1-hello.rb
@@ -4,19 +4,14 @@ ADJECTIVES = %w{stinky smelly ugly alcoholic}
 NOUNS = %w{jerk doofus dingus developer}
 
 get '/' do
-  <<-EOF
-    <h1>Welcome to Miami Ruby Brigade</h1>
-    <p><a href="/insult">get insulted here, it's free!</a></p>
-    EOF
+  erb :index
 end
 
 get '/insult' do
   insult = [ADJECTIVES.shuffle.first,
       NOUNS.shuffle.first].join ' '
-
-  <<-EOF
-  you're a #{insult}
-  EOF
+  @insult = insult
+  haml :insult, locals: {insult: insult}
 end
 
 get '/insult/:noun' do
