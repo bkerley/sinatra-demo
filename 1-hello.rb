1a8671c4fb03dec1a5d32ea4abaa2bbff61ce521
Bryce Kerley <bkerley@brycekerley.net>
2014-11-17 20:14:35 -0500

    add a json api

--- 1-hello.rb
+++ 1-hello.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 ADJECTIVES = %w{stinky smelly ugly alcoholic}
 NOUNS = %w{jerk doofus dingus developer}
@@ -20,4 +21,31 @@ get '/insult/:noun' do
   <<-EOF
   you're #{insult}, #{params[:noun]}
   EOF
+end
+
+get '/insult.json' do
+  insult = {
+    noun: (n = NOUNS.shuffle.first),
+    adjective: (a = ADJECTIVES.shuffle.first),
+    insult: [n, a].join(' ')
+  }
+  content_type :json
+  insult.to_json
+end
+
+post '/words' do
+  body = request.body.read
+  payload = JSON.parse body
+  nouns = payload['nouns']
+  adjectives = payload['adjectives']
+
+  NOUNS = [NOUNS + nouns].flatten.uniq if nouns
+  ADJECTIVES = [ADJECTIVES + adjectives].flatten.uniq if adjectives
+
+  content_type :json
+  {
+    adjectives: ADJECTIVES,
+    nouns: NOUNS,
+    payload: payload
+  }.to_json
 end
